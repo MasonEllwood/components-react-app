b0f35e5d976f2729880d092864d162791be616e5
Mason Ellwood <mason@fyresite.com>
2019-07-10 14:04:13 -0700

    details child of approval

--- src/index.js
+++ src/index.js
@@ -7,25 +7,30 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
     return (
         <div className="ui container comments">
-            <ApprovalCard />
-            <CommentDetail 
-                author="fartname1" 
-                timeAgo="Today at fartPM" 
-                commentText="Nicefart blog post!" 
-                avatar={faker.image.avatar()}
-            />
-            <CommentDetail 
-                author="fartname2" 
-                timeAgo="3 days ago at fartAM" 
-                commentText="Nice blogfart post!" 
-                avatar={faker.image.avatar()}
-            />
-            <CommentDetail 
-                author="fartname3" 
-                timeAgo="Yesterday at fartAM" 
-                commentText="Nice fart post!" 
-                avatar={faker.image.avatar()}
-            />
+            <ApprovalCard>
+                <CommentDetail 
+                    author="fartname1" 
+                    timeAgo="Today at fartPM" 
+                    commentText="Nicefart blog post!" 
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="fartname2" 
+                    timeAgo="3 days ago at fartAM" 
+                    commentText="Nice blogfart post!" 
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="fartname3" 
+                    timeAgo="Yesterday at fartAM" 
+                    commentText="Nice fart post!" 
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
         </div>
     );
 };
