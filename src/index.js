289c539f0dd296a95b59a7711a5a064611f96be2
Mason Ellwood <mason@fyresite.com>
2019-07-10 12:44:26 -0700

    added comment file and dynamic name

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
 const App = () => {
-    return <div>Hi there!</div>
+    return (
+        <div className="ui container comments">
+            <CommentDetail author="fartname1"/>
+            <CommentDetail author="fartname2"/>
+            <CommentDetail author="fartname3"/>
+        </div>
+    );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
